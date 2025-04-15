f1d552516258f4b365c6d9ad839bad89b0c4dfb7
Abhishek Bongale <abhishekbongale@outlook.com>
2025-04-15 23:56:24 +0100

    first commit of my blog

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,13 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        About Me
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Abhishek Bongale Here (short: Abhi). Keeping it simple.
+        who am I ? Enthusastic by Heart, Engineer by profession.
+        Wants to know why things works the way it works.
+        `}
       </p>
       <div className="my-8">
         <BlogPosts />
