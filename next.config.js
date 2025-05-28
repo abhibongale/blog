5e9c71c17c5159ab64aab48664a65241e7d8f81e
abhibongale <abhishekbongale@outlook.com>
2025-05-28 12:54:25 +0100

    Blog: upload the now-playing blog

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ import NextConfig from 'next';
 const nextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+
 };
 
 export default nextConfig;
